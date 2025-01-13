0c42a2f0e73315d1fbfbb6e4d01d9fd1e1465fe4
Infinite Monkey <icarogabryel2001@gmail.com>
2025-01-13 14:24:25 -0300

    Adiciona modelo de produto e configura a visualização da lista de produtos

--- src/core/urls.py
+++ src/core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from app.views import ProductListView
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ProductListView.as_view()),
 ]
